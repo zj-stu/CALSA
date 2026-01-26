522147bbe66155bbee9e81e8d49c8577f3eecd8f
zj-stu <2794577413@qq.com>
2026-01-26 16:19:08 +0800

    Update test.py

--- CALSA/test.py
+++ CALSA/test.py
@@ -43,7 +43,7 @@ from scipy.interpolate import interp1d
 from models import box_ops
 from tools.multilabel_metrics import AveragePrecisionMeter, get_multi_label
 
-from models.HAMMER import HAMMER
+from models.CALSA import HAMMER
 
 def setlogger(log_file):
     filehandler = logging.FileHandler(log_file)

360eeb761e7c1e64580596913c94e0162f01e411
zj-stu <2794577413@qq.com>
2026-06-17 20:45:03 +0800

    Update test.py

--- CALSA/test.py
+++ CALSA/test.py
@@ -369,7 +369,7 @@ if __name__ == '__main__':
     parser.add_argument('--checkpoint', default='') 
     parser.add_argument('--resume', default=False, type=bool)
     parser.add_argument('--output_dir', default='/mnt/lustre/share/rshao/data/FakeNews/Ours/results')
-    parser.add_argument('--text_encoder', default='/Home/Download/4T/zhoujun/DG/models/bert-base-uncased')
+    parser.add_argument('--text_encoder', default='.../models/bert-base-uncased')
     parser.add_argument('--device', default='cuda')
     parser.add_argument('--seed', default=777, type=int)
     # parser.add_argument('--world_size', default=1, type=int, help='number of distributed processes')    

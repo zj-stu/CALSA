28eee237feb4e33b6684ef0bb41b0eca7c4f751d
zj-stu <2794577413@qq.com>
2026-06-23 21:05:55 +0800

    Update train.sh

--- CALSA/train/train.sh
+++ CALSA/train/train.sh
@@ -1,18 +1,1 @@
-EXPID=$(date +"%Y%m%d_%H%M%S")
-export PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:128
 
-HOST='127.0.0.1'
-PORT='7'
-
-NUM_GPU=4
-python train.py \
---config 'configs/train.yaml' \
---output_dir 'results' \
---checkpoint 'ALBEF_4M.pth' \
---launcher pytorch \
---rank 0 \
---log_num ${EXPID} \
---dist-url tcp://${HOST}:1003${PORT} \
---token_momentum \
---world_size $NUM_GPU \
---model_save_epoch 100 \
